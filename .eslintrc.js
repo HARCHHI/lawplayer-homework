0d24d326def41efec4fe089051ceb25adf462640
HARCHHI <harc.maxwin@gmail.com>
2024-02-01 23:39:17 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-param-reassign': 'off',
+    'import/extensions': 'off',
+    'max-classes-per-file': 'off',
+    'class-methods-use-this': 'off',
+    'no-empty-function': 'off',
+    'no-bitwise': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'consistent-return': 'off',
+  },
+};
